48ef0de50f5cbb344985de31b7f880e55c211eb2
Henri Niva <henri.a.niva@gmail.com>
2021-04-22 02:41:19 +0300

    Go wasm stuff

---
+++ src/backend/go.mod
@@ -0,0 +1,9 @@
+module raytracer
+
+go 1.16
+
+require (
+	github.com/cosmtrek/air v1.26.0 // indirect
+	github.com/pelletier/go-toml v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20210421221651-33663a62ff08 // indirect
+)
--- src/backend/main.go
+++ src/backend/main.go
@@ -1,5 +1,17 @@
 package main
 
+//import "syscall/js"
+
 func main() {
-	println("Hello World")
+	c := make(chan struct{}, 0)
+
+	println("Go WebAssembly Initialized")
+
+	//js.Global().Set("render", js.FuncOf(render))
+
+	<-c
+}
+
+func render(this js.Value, args []js.Value) interface{} {
+	return nil
 }
