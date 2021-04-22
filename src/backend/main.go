19f0a79461628e3d0e9dee4b819375fe261972e6
Henri Niva <henri.a.niva@gmail.com>
2021-04-22 12:28:04 +0300

    Go tooling etc works

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,17 +1,18 @@
 package main
 
-//import "syscall/js"
+import "syscall/js"
 
 func main() {
-	c := make(chan struct{}, 0)
+	//c := make(chan struct{}, 0)
 
 	println("Go WebAssembly Initialized")
 
-	//js.Global().Set("render", js.FuncOf(render))
+	js.Global().Set("render", js.FuncOf(render))
 
-	<-c
+	//<-c
 }
 
 func render(this js.Value, args []js.Value) interface{} {
+	println("test")
 	return nil
 }
